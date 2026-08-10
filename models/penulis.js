0badf72e9b1ce1cbef3e925e2b6356b3d36d9830
Yuixixixixi <yendra.paundra@gmail.com>
2026-08-10 15:58:49 +0700

    Refactor Penulis model definition for improved readability and consistency

--- models/penulis.js
+++ models/penulis.js
@@ -1,38 +1,17 @@
 module.exports = (sequelize, DataTypes) => {
-  const Penulis = sequelize.define(
-    'Penulis',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      nama: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    {
-      tableName: 'penulis',
-      timestamps: true,
-    }
-  );
- Penulis.associate = (models) => {
-  Penulis.hasMany(models.Komik, {
-    foreignKey: 'penulisId',
-    as: 'buku',
+  const Penulis = sequelize.define('Penulis', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    nama: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false }
+  }, {
+    tableName: 'penulis',
+    timestamps: true
   });
-};
 
-return Penulis;
-};
+  Penulis.associate = (models) => {
+    Penulis.hasMany(models.Komik, { foreignKey: 'penulis_id', as: 'buku' });
+  };
 
+  return Penulis;
+};
